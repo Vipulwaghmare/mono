650cbfb328c035da102f8b420604be7feef6d592
vipul <vipulwaghmare222@gmail.com>
2025-03-28 19:12:22 +0530

    cookieparser issue fix

--- packages/backend-nestjs/src/main.ts
+++ packages/backend-nestjs/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import * as fs from 'fs';
 import * as path from 'path';
 
