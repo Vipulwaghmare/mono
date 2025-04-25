cd022e816ca17866761dd4c0b62aec3d2e561362
vipul <vipulwaghmare222@gmail.com>
2025-04-25 15:31:13 +0530

    updated google tag in portfolio

--- packages/marathi-lyrics/src/app/search/page.tsx
+++ packages/marathi-lyrics/src/app/search/page.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
 import { SearchBar } from "@/components/search-bar";
-import { searchSongs } from "@/lib/data";
+import { SongTab } from "@/components/song-tab";
+import api from "@/lib/api";
 
 export default async function SearchPage({
   searchParams,
@@ -8,42 +8,13 @@ export default async function SearchPage({
   searchParams: { q: string };
 }) {
   const query = searchParams.q || "";
-  const songs = await searchSongs(query);
+  const songs = (await api.marathiControllerGetSong()).data;
 
   return (
     <div className="container">
       <h1 className="section-title">{`Search Results for "${query}"`}</h1>
-
       <SearchBar defaultValue={query} />
-
-      <div className="song-list">
-        {songs.length > 0 ? (
-          songs.map((song) => (
-            <Link
-              href={`/songs/${song.id}`}
-              key={song.id}
-              className="song-card"
-            >
-              <div className="song-card-content">
-                <h3 className="song-title">{song.name}</h3>
-                <div className="song-meta">
-                  <span>Singer: {song.singer}</span>
-                  <span>Lyricist: {song.lyricist}</span>
-                </div>
-                <div className="song-tags">
-                  {song.tags.map((tag, index) => (
-                    <span key={index} className="song-tag">
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            </Link>
-          ))
-        ) : (
-          <p>{`No songs found matching "${query}"`}</p>
-        )}
-      </div>
+      <SongTab songs={songs} letter={query} />
     </div>
   );
 }
