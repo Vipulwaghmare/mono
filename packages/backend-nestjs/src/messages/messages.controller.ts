096cfdefa9e6cbc525f1b88b479ba92ed4052aa4
vipul <vipulwaghmare222@gmail.com>
2025-05-21 12:01:58 +0530

    fixed issue

---
+++ packages/backend-nestjs/src/messages/messages.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, InternalServerErrorException, Logger, Param, Post } from '@nestjs/common';
+import { ContactMeDto } from './dtos/create-messages.dto';
+import { EmailService } from '../services/email.service';
+
+@Controller('/api/v1/message')
+export class MessagesController {
+  private readonly logger = new Logger(MessagesController.name);
+  constructor(
+    private readonly emailService: EmailService,
+  ) { }
+
+  @Post('/contact-me')
+  async createMessage(@Body() body: ContactMeDto) {
+    try {
+      this.logger.log({ body }, 'Sending contac me message');
+      await this.emailService.sendEmail({
+        to: 'vipulwaghmare222@gmail.com',
+        subject: `${body.email}  ${body.subject}`,
+        text: body.message,
+      })
+      return {
+        message: 'Successfully sent contact me message',
+      }
+    } catch {
+      throw new InternalServerErrorException('Failed to send contact me message');
+
+    }
+  }
+}
