cbe05aca15060f5e11a3a60fa1c4b28b025d3051
vipul <vipulwaghmare222@gmail.com>
2025-06-03 14:47:44 +0530

    blogs style update

--- packages/blogs/src/app/blogs/this-keyword/page.tsx
+++ packages/blogs/src/app/blogs/this-keyword/page.tsx
@@ -9,25 +9,23 @@ export default function ThisKeyword() {
           In JS, this keyword refers to an object, but which object? Well it
           depends on where the this is getting executed.
         </p>
-        <p>Let's understand with code examples because that is easy XD.</p>
+        <p>{`Let's understand with code examples because that is easy XD.`}</p>
         <h3>1. Global this</h3>
         <p>
           What is value of this, if you just console.log it? Well, it depends on
           where you are running your code
         </p>
-        <p>In browser => It's a window object</p>
-        <p>In NodeJs => It's a global object</p>
-        <p>In the next part we will talk in the browser context, so if you saw that this is global. Just remember that it'll be global object for nodejs.</p>
+        <p>{`In browser => It's a window object`}</p>
+        <p>{`In NodeJs => It's a global object`}</p>
+        <p>{`In the next part we will talk in the browser context, so if you saw that this is global. Just remember that it'll be global object for nodejs.`}</p>
         <h3>2. Inside a function declared with function keyword</h3>
-        <CodeBlock>{
-          `function test() {
+        <CodeBlock>{`function test() {
   console.log(this);
 }
 test(); // window (non-strict), undefined (strict)
-`
-}</CodeBlock>
+`}</CodeBlock>
         <p>Technically, this</p>
-        <p>What I say is look at the parent of what function, basically what is outside of curly braces of the function. Yeah it's this</p>
+        <p>{`What I say is look at the parent of what function, basically what is outside of curly braces of the function. Yeah it's this`}</p>
       </div>
       <div>
         <CodeBlock>
