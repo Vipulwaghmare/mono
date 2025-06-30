1d1c7f8e21a2e59184974a2d3f21c2e73729e8b6
vipul <vipulwaghmare222@gmail.com>
2025-06-30 13:20:42 +0530

    astro setting update

--- packages/portfolio/astro.config.mjs
+++ packages/portfolio/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://vipulwaghmare.com",
+  integrations: [sitemap()],
+  build: {
+    format: "directory",
+  },
+});
