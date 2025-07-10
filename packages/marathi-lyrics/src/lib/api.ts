8585c5b10dff9bf00e0d58ec348cb575ab5fa104
vipul <vipulwaghmare222@gmail.com>
2025-07-10 13:03:38 +0530

    removed unused projects

--- packages/marathi-lyrics/src/lib/api.ts
+++
@@ -1,7 +0,0 @@
-import { AuthApi, MarathiApi } from "@vipulwaghmare/apis";
-import { axiosConfig } from "./axios-instance";
-
-const api = new MarathiApi(axiosConfig, undefined); // Create an API instance
-export const authApi = new AuthApi(axiosConfig, undefined)
-
-export default api;
